166aa8c9d7b202bdf412ca61919acf3cc1a0214f
CooperJRG <cooperjgilkey@gmail.com>
2023-10-28 13:26:03 -0500

    Accurate constants for V0

--- src/C/genes/gene_encoding.h
+++ src/C/genes/gene_encoding.h
@@ -3,27 +3,30 @@
 
 #include <stdint.h>
 
-#define NUM_NEURONS 20
+#define NUM_SENSORY_NEURONS 16
+#define NUM_INTERNAL_NEURONS 5
+#define NUM_CONSTANT_NEURONS 0
+#define NUM_ACTION_NEURONS 9
 #define NUM_ACTIVATION_FUNCTIONS 3
 
 // Structure to hold gene details
 typedef struct {
     uint64_t gene;         // 64 bits for the gene
     /* 
-    First two bits is for the input type
+    First bits is for the input type
         00 - 0: Sensory input
-        01 - 1: Hidden input
+        01 - 1: Internal input
         10 - 2: Constant input
         11 - 3: Reserved for future use
     Next 10 bits is for the source neuron ID
-        Takes values from 0 to 1023 % num_neurons
+        Takes values from 0 to 1023 % num_corresponding_neurons
     Next 2 bits is for the output type
         00 - 0: Action output
-        01 - 1: Hidden output
+        01 - 1: Internal output
         10 - 2: Reserved for future use
         11 - 3: Reserved for future use
     Next 10 bits is for the destination neuron ID
-        Takes values from 0 to 1023 % num_neurons
+        Takes values from 0 to 1023 % num_corresponding_neurons
     Next 24 bits is for the weight, signed
         Takes values from -8388608 to 8388607 / 2097151
         Roughly -4 to 4
