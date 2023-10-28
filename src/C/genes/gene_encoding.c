2c4af063e693fc99a54fa93ae134e34258f3aa89
CooperJRG <cooperjgilkey@gmail.com>
2023-10-28 13:25:30 -0500

    Update representation to account for overlap

--- src/C/genes/gene_encoding.c
+++ src/C/genes/gene_encoding.c
@@ -12,8 +12,9 @@ uint16_t get_source_neuron_id(const Gene* gene) {
     // Use bit manipulation to extract the next 10 bits
     uint16_t bit_source_id = gene->gene >> 52;
     bit_source_id = bit_source_id & 0x3FF;
-    // Convert the 10 bits by taking the modulus with respect to the constant number of neurons
-    uint16_t source_id = bit_source_id % NUM_NEURONS;
+    uint16_t num_neurons, offset;
+    get_source_num_corresponding_neurons_and_offset(gene, &num_neurons, &offset);
+    uint16_t source_id = (bit_source_id % num_neurons) + offset;
     return source_id;
 }
 
@@ -31,7 +32,9 @@ uint16_t get_destination_neuron_id(const Gene* gene) {
     uint16_t bit_output_id = gene->gene >> 40;
     bit_output_id = bit_output_id & 0x3FF;
     // Convert the 10 bits by taking the modulus with respect to the constant number of neurons
-    uint16_t output_id = bit_output_id % NUM_NEURONS;
+    uint16_t num_neurons, offset;
+    get_output_num_corresponding_neurons_and_offset(gene, &num_neurons, &offset);
+    uint16_t output_id = (bit_output_id % num_neurons) + offset;
     return output_id;
 }
 
@@ -53,4 +56,44 @@ uint8_t get_activation_function(const Gene* gene) {
     // Convert the 10 bits to a signed integer by taking the modulus with respect to the constant number of neurons
     uint8_t activation = bit_activation % NUM_ACTIVATION_FUNCTIONS;
     return activation;
+}
+
+// Helper method to get the number of corresponding source neurons and offset based on the input type
+void get_source_num_corresponding_neurons_and_offset(const Gene* gene, uint16_t* num_neurons, uint16_t* offset) {
+    uint8_t input_type = get_input_type(gene);
+    switch(input_type) {
+        case 0:  // Sensory input
+            *num_neurons = NUM_SENSORY_NEURONS;
+            *offset = 0;
+            break;
+        case 1:  // Hidden input
+            *num_neurons = NUM_INTERNAL_NEURONS;
+            *offset = NUM_SENSORY_NEURONS;
+            break;
+        case 2:  // Constant input
+            *num_neurons = NUM_CONSTANT_NEURONS;
+            *offset = NUM_SENSORY_NEURONS + NUM_INTERNAL_NEURONS;
+            break;
+        default:
+            *num_neurons = 0;
+            *offset = 0;  // Handle error or reserved types
+    }
+}
+
+// Helper method to get the number of corresponding source neurons and offset based on the output type
+void get_output_num_corresponding_neurons_and_offset(const Gene* gene, uint16_t* num_neurons, uint16_t* offset) {
+    uint8_t output_type = get_output_type(gene);  // Changed from input_type to output_type
+    switch(output_type) {
+        case 0:  // Action output
+            *num_neurons = NUM_ACTION_NEURONS;
+            *offset = NUM_SENSORY_NEURONS + NUM_INTERNAL_NEURONS + NUM_CONSTANT_NEURONS;
+            break;
+        case 1:  // Hidden output
+            *num_neurons = NUM_INTERNAL_NEURONS;
+            *offset = NUM_SENSORY_NEURONS;
+            break;
+        default:
+            *num_neurons = 0;
+            *offset = 0;  // Handle error or reserved types
+    }
 }
